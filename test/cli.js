0a4cbb199d420e78ab52ee63ec0d6849df8df3b1
Mark Griffiths <mark@thebespokepixel.com>
2021-11-05 23:16:25 +0000

    Update Dependencies

--- test/cli.js
+++ test/cli.js
@@ -11,3 +11,18 @@ test(`Module name/version is '${pkg.name} v${expectedVersion}'.`, async t => {
 	const {stdout} = await execPromise('./trucolor.js -vv')
 	t.is(stdout.trim(), `${pkg.name} v${expectedVersion}`)
 })
+
+test('Module help, no color', async t => {
+	const {stderr} = await execPromise('./trucolor.js --help --no-color')
+	t.snapshot(stderr)
+})
+
+test('Module special help, no color', async t => {
+	const {stderr} = await execPromise('./trucolor.js --help special --no-color')
+	t.snapshot(stderr)
+})
+
+test('Module named help, no color', async t => {
+	const {stderr} = await execPromise('./trucolor.js --help named --no-color')
+	t.snapshot(stderr)
+})
